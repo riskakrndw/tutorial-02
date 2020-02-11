83da5e872da09a75e2e79e341e2e75cb13bb7507
Riska Kurnia Dewi <riskakurniad@gmail.com>
2020-02-11 22:16:41 +0700

    adding converting number

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -37,12 +37,46 @@ public class PageController{
     //     return "hello2";
     // }
 
+    // @RequestMapping("/calcuconvert")
+    // public String calcuconvert(@RequestParam(value = "bilPertama", required = false) String bilPertama, @RequestParam(value = "bilKedua", required = false) String bilKedua, Model model){
+    //     int hasil = Integer.parseInt(bilPertama) + Integer.parseInt(bilKedua);
+    //     model.addAttribute("bilPertama", bilPertama);
+    //     model.addAttribute("bilKedua", bilKedua);
+    //     model.addAttribute("hasil", hasil);
+    //     return "calcuconvert";
+    // }
+
     @RequestMapping("/calcuconvert")
     public String calcuconvert(@RequestParam(value = "bilPertama", required = false) String bilPertama, @RequestParam(value = "bilKedua", required = false) String bilKedua, Model model){
         int hasil = Integer.parseInt(bilPertama) + Integer.parseInt(bilKedua);
         model.addAttribute("bilPertama", bilPertama);
         model.addAttribute("bilKedua", bilKedua);
         model.addAttribute("hasil", hasil);
+
+        String[] numbers = { "", " Satu", " Dua", " Tiga", " Empat", " Lima", " Enam", " Tujuh", " Delapan", " Sembilan", " Sepuluh" };
+        String[] dozens = { "", " Belas", " Dua Puluh", " Tiga Puluh", " Empat Puluh", " Lima Puluh", " Enam Puluh", " Tujuh Puluh", " Delapan Puluh", " Sembilan Puluh" };
+        String[] hundreds= { "", " Seratus", " Dua Ratus", " Tiga Ratus", " Empat Ratus", " Lima Ratus", " Enam Ratus", " Tujuh Ratus", " Delapan Ratus", " Sembilan Ratus" };
+        String[] thousands = { "", " Seribu", " Dua Ribu", " Tiga Ribu", " Empat Ribu", " Lima Ribu", " Enam Ribu", " Tujuh Ribu", " Delapan Ribu", " Sembilan Ribu" };
+
+        if (hasil < 9999 && hasil > 0) {
+            int satuan, puluhan, ratusan, ribuan;
+            satuan= hasil % 10;
+            puluhan= (hasil % 100) / 10;
+            ratusan= (hasil % 1000) / 100;
+            ribuan= hasil / 1000;
+            if (puluhan== 1) {
+                if (satuan == 1) {
+                    model.addAttribute("bilangan", thousands[ribuan] + hundreds[ratusan] + "Se" + dozens[puluhan]);
+                } else {
+                    model.addAttribute("bilangan", thousands[ribuan] + hundreds[ratusan] + numbers[satuan] + dozens[puluhan]);
+                } 
+            } else {
+                model.addAttribute("bilangan", thousands[ribuan] + hundreds[ratusan] + dozens[puluhan] + numbers[satuan]);
+            }
+        } else {
+            model.addAttribute("bilangan", "Converting failed");
+        }
+
         return "calcuconvert";
     }
 }
